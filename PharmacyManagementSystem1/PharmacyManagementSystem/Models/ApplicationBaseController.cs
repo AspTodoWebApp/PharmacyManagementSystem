114b5b33bb2abcb20d8a38d96d7f4597ae5b41ca
NumanArshad <37812712+NumanArshad@users.noreply.github.com>
2018-11-21 21:40:20 +0500

    Complete Login with change and forget password

---
+++ PharmacyManagementSystem1/PharmacyManagementSystem/Models/ApplicationBaseController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PharmacyManagementSystem.Models;
+using Microsoft.AspNet.Identity;
+
+namespace PharmacyManagementSystem.Models
+{
+    public class ApplicationBaseController : Controller
+    {
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (User != null)
+            {
+                var context = new PharmacyDBEntities2();
+                var userid = User.Identity.GetUserId();
+                var username = context.AspNetUsers.Where(x => x.Id == userid).First().AccountUserName;
+                var currentdate = DateTime.Today.ToString("yyyy-MM-dd");
+                var MedicineAddedToday = context.Stocks.Where(x => x.AddedDate.ToString() == currentdate).Count();
+                if (!string.IsNullOrEmpty(username) || MedicineAddedToday>=0)
+                {
+                    ViewData.Add("FullName", username);
+                    ViewData.Add("MedicineAddToday", MedicineAddedToday);
+
+                }
+            }
+            base.OnActionExecuted(filterContext);
+        }
+        public ApplicationBaseController()
+        { }
+    }
+}
